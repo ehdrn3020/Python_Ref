1689db5f26db171ab6284f2e1f43b41e07f6ba32
DongGu Kang <ehdrnsla3020@naver.com>
2017-02-07 11:10:54 +0900

    Add files via upload

---
+++ print_Star.py
@@ -0,0 +1,14 @@
+﻿n = 1
+for i in range(0,5):
+	for j in range(n):
+		print('*',end='')
+	n+=1
+	print("")
+
+#reverse
+n = 5
+for i in range(0,5):
+	for j in range(n):
+		print('*',end='')
+	n-=1
+	print("")
