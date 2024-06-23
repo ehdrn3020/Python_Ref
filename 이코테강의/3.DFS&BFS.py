8870d829a2f43886b266c173dad5c632301d6890
ehdrn3020 <ehdrnsla3020@naver.com>
2024-06-23 18:03:23 +0900

    DFS 코드 수정

--- 이코테강의/3.DFS&BFS.py
+++ 이코테강의/3.DFS&BFS.py
@@ -166,20 +166,19 @@ def bfs(x, y):
         x, y = queue.popleft()
         # 현재 위치에서 4가지 방향으로의 위치 확인
         for i in range(4):
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                # 미로 찾기 공간을 벗어난 경우 무시
-                if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                    continue
-                # 벽인 경우 무시
-                if graph[nx][ny] == 0:
-                    continue
-                # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = graph[x][y] + 1
-                    queue.append((nx, ny))
-                # 가장 오른쪽 아래까지의 최단 거리 반환
-            return graph[n - 1][m - 1]
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if graph[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    return graph[n - 1][m - 1]
 
 print(bfs(0, 0))
